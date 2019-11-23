8b17a4396678ef8f1dab0c833db1f1c436f3203e
tier777 <tier1993@gmail.com>
2019-11-23 23:31:21 +0300

    Initial Commit

---
+++ SampleFramework/SampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  SampleFramework.h
+//  SampleFramework
+//
+//  Created by Nikita Gorobets on 23.11.2019.
+//  Copyright © 2019 ng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleFramework.
+FOUNDATION_EXPORT double SampleFrameworkVersionNumber;
+
+//! Project version string for SampleFramework.
+FOUNDATION_EXPORT const unsigned char SampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFramework/PublicHeader.h>
+
+
